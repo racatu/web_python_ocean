4808dbf7dc66cba2cff5da710281d7bb5040f821
racatu <m47h3u55m@gmail.com>
2023-02-15 22:40:48 +0000

    fim da aula 2

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect, session
 
 app = Flask(__name__)
+
+SECRET_KEY = "mousse"
+app.config.from_object(__name__)
 
 #POSTS MOCK
 posts = [
@@ -18,7 +21,22 @@ posts = [
     }
 ]
 
+#USER MOCKS
+USERNAME = "admin"
+PASSWORD = "admin"
+
 @app.route('/')
 def exibir_entradas():
 
     return render_template("exibir_entradas.html", entradas=posts)
+
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    erro = ""
+    if request.method == "POST":
+        if request.form['username'] == USERNAME and request.form['password'] == PASSWORD:
+            session['logado'] = True
+            return redirect(url_for('exibir_entradas'))
+        erro = "Usuário ou senha inválidos"
+    return render_template("login.html", erro=erro)
